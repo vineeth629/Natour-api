ec8378a54c75c1b0669b63378972963e184aaef4
Vineeth <vineeth67229@gmail.com>
2026-08-13 19:50:24 +0530

    Fouth commit with post request

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+app.use(express.json());
 /*app.get('/',(req,res)=>{//for receiving the request
     res.status(200).send("Hello from the server");//res.status() is for status 
 })
@@ -13,13 +14,28 @@ const tours = JSON.parse(
 app.get('/api/v1/tours',(req,res)=>{
     res.status(200).json({
         status : 'success',
+        results : tours.length,//tours is a array so it make sense 
         data:{
             tours
         }
     })
 })
-app.post('/',(req,res)=>{
+/*app.post('/',(req,res)=>{
     res.send("you can post to this endpoint.....");
+})*/
+app.post('/api/v1/tours',(req,res)=>{
+    //console.log(res.body);
+    const newId = tours[tours.length-1].id+1;
+    const newTour = Object.assign({id: newId}, req.body);
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/tours-simple.json`,JSON.stringify(tours,null,2),err=>{
+        res.status(201).json({
+            status : "Success",
+            data :{
+                tour : newTour
+            }
+        })
+    })
 })
 const port = 3000;
 app.listen(port,()=>{//for sending the request
