7a6a364fbfa281dff38ba282c76ef13f499697b6
Vineeth <vineeth67229@gmail.com>
2026-08-21 14:29:50 +0530

    Param middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,15 @@ const fs = require('fs');
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/tours-simple.json`)
 );
-
+exports.checkID = (req,res,next,val)=>{
+      if((req.params.id*1)>tours.length){
+        return res.status(400).json({
+          status : 'fail',
+          message : 'invalid id'
+        });
+      }
+      next();
+}
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -16,16 +24,7 @@ exports.getAllTours = (req, res) => {
 
 
 exports.getTour = (req, res) => {
-  const id = req.params.id * 1;
-
-  const tour = tours.find(el => el.id === id);
-
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    });
-  }
+ 
 
   res.status(200).json({
     status: 'success',
@@ -63,18 +62,7 @@ exports.createTour = (req, res) => {
 
 
 exports.updateTour = (req, res) => {
-  const id = req.params.id * 1;
-
-  const tour = tours.find(el => el.id === id);
-
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    });
-  }
-
-  res.status(200).json({
+   res.status(200).json({
     status: 'success',
     data: {
       tour: 'Updated data here...'
@@ -84,16 +72,5 @@ exports.updateTour = (req, res) => {
 
 
 exports.deleteTour = (req, res) => {
-  const id = req.params.id * 1;
-
-  const tour = tours.find(el => el.id === id);
-
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    });
-  }
-
   res.status(204).send();
 };
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,114 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const fs = require('fs');
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/tours-simple.json`)
-);
-
-const getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    requestedAt: req.requestTime,
-    data: {
-      tours
-    }
-  });
-};
-
-
-const getTour = (req, res) => {
-  const id = req.params.id * 1;
-
-  const tour = tours.find(el => el.id === id);
-
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    });
-  }
-
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    data: {
-      tour
-    }
-  });
-};
-
-
-const createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-
-  const newTour = Object.assign(
-    { id: newId },
-    req.body
-  );
-
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/tours-simple.json`,
-    JSON.stringify(tours, null, 2),
-    err => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour
-        }
-      });
-    }
-  );
-};
-
-
-const updateTour = (req, res) => {
-  const id = req.params.id * 1;
-
-  const tour = tours.find(el => el.id === id);
-
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    });
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: 'Updated data here...'
-    }
-  });
-};
-
-
-const deleteTour = (req, res) => {
-  const id = req.params.id * 1;
-
-  const tour = tours.find(el => el.id === id);
-
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    });
-  }
-
-  res.status(204).send();
-};
+const tourHandler = require('./../controllers/tourController.js');
+router.param('id',tourHandler.checkID);//example of param middleware
 router
   .route('/')
-  .get(getAllTours)
-  .post(createTour);
+  .get(tourHandler.getAllTours)
+  .post(tourHandler.createTour);
 
 router
   .route('/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
+  .get(tourHandler.getTour)
+  .patch(tourHandler.updateTour)
+  .delete(tourHandler.deleteTour);
 
   module.exports = router;
