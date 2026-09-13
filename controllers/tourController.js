4bd6c1fc623d5ff6d3f2d86bfca54b7d5d3816f2
Vineeth <vineeth67229@gmail.com>
2026-09-13 18:49:15 +0530

    Mongoose basics

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,29 +1,86 @@
 const Tour = require('./../models/tourModel.js');
 
-exports.getAllTours = (req, res) => {
+exports.getAllTours = async (req, res) => {
+  try{
+  const tours = await Tour.find()
   res.status(200).json({
-    status: 'success'
+    status: 'success',
+    results: tours.length,
+    data:{
+      tours
+    }
   });
+}catch(err){
+  res.status(404).json({
+    status:'Failed',
+    message : err
+  });
+}
 };
 
-exports.getTour = (req, res) => {
-  res.status(200).json({
-    status: 'success'
+exports.getTour = async (req, res) => {
+  try{
+    const tours = await Tour.findById(req.params.id);
+    //Tour.findOne({_id: req.params.id})
+     res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data:{
+      tours
+    }
   });
+
+  }catch(err){
+     res.status(404).json({
+    status:'Failed',
+    message : err
+  });
+  }
 };
 
-exports.createTour = (req, res) => {
+exports.createTour = async (req, res) => {
+
+   //const newTour = new Tour({})
+   //newTour.save()
+   const newTour = await Tour.create(req.body);//newTour is newly created document
+
+
   res.status(201).json({
     status: 'success'
   });
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success'
+exports.updateTour = async (req, res) => {
+  try{
+    const tour  = await Tour.findByIdAndUpdate(req.params.id,req.body,{
+      new : true,
+      runValidators : true
+    });
+    res.status(200).json({
+    status: 'success',
+    data:{
+      tour
+    }
   });
+  }catch(err){
+    res.status(404).json({
+      status:'failed',
+      message: err
+    });
+  }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).send();
+exports.deleteTour = async (req, res) => {
+  try{
+    const tours = await Tour.findByIdAndDelete(req.params.id)
+    res.status(204).json({
+    status: 'success',
+    data : null
+  });
+  }catch(err){
+       res.status(404).json({
+      status:'failed',
+      message: err
+    });
+  }
 };
